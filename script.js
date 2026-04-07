5893cf30d7f26c024c99fbf117316ffd36f707e1
ahmadkiflan <ahmadkiflan2020@gmail.com>
2026-04-08 02:26:41 +0700

    delete many project

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-function upperCaseString(fullName) {
-  return fullName.toUpperCase();
+const inputAdd = document.getElementById("inputAdd");
+const ul = document.getElementsByTagName("ul")[0];
+
+function addTask() {
+  if (inputAdd.value == "") {
+    alert("lu belum isi sesuatu di input!");
+    saveData();
+  } else {
+    const li = document.createElement("li");
+    li.innerHTML = inputAdd.value;
+    ul.appendChild(li);
+    const span = document.createElement("span");
+    span.innerHTML = "&#10006";
+    li.appendChild(span);
+    inputAdd.value = "";
+  }
+  saveData();
 }
 
-function addFullName(x) {
-  console.log("Halo " + upperCaseString(x));
+ul.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName == "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("data", ul.innerHTML);
 }
 
-addFullName("Ahmad Kiflan Wafi");
-
-const result = upperCaseString("Ahmad Kiflan Wafi");
-console.log(result);
+function showData() {
+  ul.innerHTML = localStorage.getItem("data");
+}
+showData();
